8c7867135ae0e2bd88b98917e91f7614427c974e
Timofey Kochetkov <pochta30052001@gmail.com>
2020-12-25 20:27:48 +0400

    update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -12,15 +12,44 @@ formElem.addEventListener("formdata", event => {
   const data = event.formData;
 
   const entries = [...data.entries()];
-  console.log(entries);
-
   const values = [...data.values()];
-  console.log(values);
+  if(validate(values[0])) {
+    console.log(entries);
+    console.log(values);
+  
+    Swal.fire({
+      icon: 'success',
+      title: 'done',
+      text: 'Данные отправлены',
+    });
+    console.log("Все норм, данные отправлены");
+  }
+  else {
+    Swal.fire({
+      icon: 'error',
+      title: 'Что-то пошло не так',
+      text: 'Неправильно указан номер',
+    });
+    console.log("Все плохо, данные не отправлены");
+  }
 
-  Swal.fire({
-    icon: 'success',
-    title: 'done',
-    text: 'Данные отправлены',
-  });
-  console.log("Все норм, данные отправлены");
+  
 });
+
+
+function validate(tel){
+  let sub = "+7";
+
+  if (!tel.includes(sub)){
+      Swal.fire({
+          icon: 'error',
+          title: 'Что-то пошло не так',
+          text: 'Неправильно указан номер',
+        });
+        console.log("Косяк в номере");
+      return false;
+  }
+  else{
+      return true;
+  }
+}
