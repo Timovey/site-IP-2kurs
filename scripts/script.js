ffba9c855632362c2f4066342b404088e51228dc
Timofey Kochetkov <pochta30052001@gmail.com>
2020-12-25 20:17:44 +0400

    create FormData and output in console

--- scripts/script.js
+++ scripts/script.js
@@ -1,56 +1,26 @@
-$(document).ready(()=>{
-    $('form').submit((event)=>{
-        let tel = $('#inputTel3').val();
-        let name = $('#inputName3').val();
-        let family = $('#inputFamily3').val();
-        let pit = $('#inlineFormCustomSelect').val();
-        event.preventDefault();
-        if(validate(tel, name, family, pit)){
-            Swal.fire({
-                icon: 'success',
-                title: 'done',
-                text: 'Данные отправлены',
-              });
-              console.log("Все норм, данные отправлены");
-        }
-        else {
-            console.log("Кто-то накосячил");
-        };
-        
-    });
+const formElem = document.forms[0];
+
+formElem.addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    new FormData(formElem);
+
+  });
+
+
+formElem.addEventListener("formdata", event => {
+  const data = event.formData;
+
+  const entries = [...data.entries()];
+  console.log(entries);
+
+  const values = [...data.values()];
+  console.log(values);
+
+  Swal.fire({
+    icon: 'success',
+    title: 'done',
+    text: 'Данные отправлены',
+  });
+  console.log("Все норм, данные отправлены");
 });
-
-function validate(tel, name, family, pit){
-    let sub = "+7";
-    if ((empty(name)||empty(family)||empty(pit))){
-        Swal.fire({
-            icon: 'error',
-            title: 'Что-то пошло не так',
-            text: 'Заполните поля',
-          });
-          console.log("Косяк в пустых полях");
-        return false;
-    }
-    if (!tel.includes(sub)){
-        Swal.fire({
-            icon: 'error',
-            title: 'Что-то пошло не так',
-            text: 'Неправильно указан номер',
-          });
-          console.log("Косяк в номере");
-        return false;
-    }
-    else{
-        return true;
-    }
-}
-
-function empty(znach){
- if (znach == ""){
-     return true;
- }
- else{
-     return false;
- }
-}
-
